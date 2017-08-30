7fdc7277ee1c3f897ebdb8c2e52d3c50f289ca05
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-08-30 07:47:19 +0700

    Product Detail: Fix some errors in fetching data

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,30 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit }        from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Product }          from '../shared/product.model';
+import { Seller  }          from '../shared/seller.model';
 import { ProductService }   from '../shared/product.service';
 import { SellerService }    from '../shared/seller.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'app-product-detail',
     templateUrl: './product-detail.component.html',
-    styleUrls: [],
+    styleUrls: [ './product-detail.component.css' ],
 })
 export class ProductDetailComponent {
+
+    product: Product;
+    seller: Seller;
 
     constructor(
         private productService: ProductService,
         private sellerService: SellerService,
-    )
+        private route: ActivatedRoute,
+    ) {  }
 
     ngOnInit(): void {
         this.route.paramMap
-            .switchMap((params: ParamMap) => return fetchProduct(+params.get('id')))
+            .switchMap((params: ParamMap) => this.fetchProduct(params.get('id')))
             .subscribe((product) => {
+                console.log('product = ', product);
                 this.product = product;
 
-                fetchSeller(product.seller_id)
-                    .then((seller) => this.seller = seller));
+                this.fetchSeller(product.seller_id)
+                    .then((seller) => this.seller = seller);
             });
     }
 
@@ -32,7 +40,7 @@ export class ProductDetailComponent {
      * Fetch specific product
      * @param {Number} id - product id
      */
-    fetchProduct(id: number): Promise<Product> {
+    fetchProduct(id: string): Promise<Product> {
         return this.productService.getProduct(id);
     }
 

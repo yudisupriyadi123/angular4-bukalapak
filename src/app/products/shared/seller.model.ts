12475bbc7a2f439a55b021c81315f134d6df629a
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-08-30 07:50:22 +0700

    SellerModel: Add new property

--- src/app/products/shared/seller.model.ts
+++ src/app/products/shared/seller.model.ts
@@ -1,11 +1,13 @@
 export class Seller {
-    id         : number;
-    username   : string;
-    name       : string;
-    avatar     : string;
-    last_login : string;
-    joined_at  : string;
-    address    : {province : string, city : string};
+    id            : number;
+    username      : string;
+    name          : string;
+    avatar        : string;
+    last_login    : string;
+    joined_at     : string;
+    address       : {province : string, city : string};
+    delivery_time : string;
+    feedbacks     : {positive : number, negative : number};
     // actually, bukalapak returns many data over this
     // but since the others is not needed, so these data is enough
 }

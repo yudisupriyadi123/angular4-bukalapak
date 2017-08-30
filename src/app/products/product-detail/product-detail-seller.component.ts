eb67130a8a9b0fac3adea5b749a118c5acc09e1b
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2017-08-30 07:43:43 +0700

    Product Detail: Fix missing error

--- src/app/products/product-detail/product-detail-seller.component.ts
+++ src/app/products/product-detail/product-detail-seller.component.ts
@@ -1,11 +1,11 @@
-import { Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Seller }   from '../shared/seller.model';
 
 @Component({
     selector: 'app-product-detail-seller',
     templateUrl: './product-detail-seller.component.html',
-    styleUrls: [],
+    styleUrls: [ './product-detail-seller.component.css' ],
 })
 export class ProductDetailSellerComponent {
     @Input() seller : Seller;
